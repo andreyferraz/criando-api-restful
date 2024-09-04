e5ff6dea070cebb85881334877e1119b295a2743
andreyferraz <andreyferraz@unimedceara.com.br>
2024-09-04 02:01:51 -0300

    updates in class swagger

--- src/main/java/com/example/criandoapis/config/SwaggerConfig.java
+++ src/main/java/com/example/criandoapis/config/SwaggerConfig.java
@@ -2,22 +2,16 @@ package com.example.criandoapis.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
+import org.springdoc.core.GroupedOpenApi;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.criandoapis"))
-                .paths(PathSelectors.any())
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-apis")
+                .pathsToMatch("/**")
                 .build();
     }
 }
