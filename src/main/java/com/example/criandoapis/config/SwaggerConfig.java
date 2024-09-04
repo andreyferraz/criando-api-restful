bb9249d61c7fc994e65024f512059d97657477bc
andreyferraz <andreyferraz@unimedceara.com.br>
2024-09-04 02:23:41 -0300

    adjusts in swagger doc

--- src/main/java/com/example/criandoapis/config/SwaggerConfig.java
+++ src/main/java/com/example/criandoapis/config/SwaggerConfig.java
@@ -1,8 +1,8 @@
 package com.example.criandoapis.config;
 
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springdoc.core.GroupedOpenApi;
 
 @Configuration
 public class SwaggerConfig {
