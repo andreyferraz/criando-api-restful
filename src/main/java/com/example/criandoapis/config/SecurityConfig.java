1650ad379a533e843c820a4c54e3ce4e4da4d9bd
andreyferraz <andreyferraz@unimedceara.com.br>
2024-09-04 02:07:47 -0300

    remove security access to route swagger doc

--- src/main/java/com/example/criandoapis/config/SecurityConfig.java
+++ src/main/java/com/example/criandoapis/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.criandoapis.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,7 +17,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/api/users/**").authenticated()
+                        .anyRequest().permitAll()  // Permite todas as outras requisições
                 )
                 .httpBasic();
         return http.build();
